60738cbd9ee727c6c7f3057b9e06e91a139a900a
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-12-02 11:01:42 -0500

    Add files via upload
    Constructed an algorithm to figure out if two numbers are even or odd using Python

---
+++ Even_Odd_Algorithm .py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def even_odd (x , y):
+    if x%2== 0:
+        print('Even')
+    else:
+        print('Odd')
+    if y%2 == 0:
+        print('Even')
+    else:
+        print("Odd")
+class main ():
+    even_odd(10000 ,550)
+
