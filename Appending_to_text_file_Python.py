6047235af0125cbb0a330dffd28ea9a3d3e2f8a3
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-12-12 20:08:41 -0500

    Add files via upload
    Add content to a preexisting file

---
+++ Appending_to_text_file_Python.py
@@ -0,0 +1,7 @@
+def main():
+    document = open("document for christmas list","a")
+
+    document.write("\n"+"Lets add things to the document..")
+    document.close()  #Closed the document
+
+main()  #Called the main function
